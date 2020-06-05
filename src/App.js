65b438a15553cd283d0d18aba45ace5ccafdac46
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-05 16:55:33 +1000

    consume context in app -> tracklist

--- src/App.js
+++ src/App.js
@@ -2,17 +2,20 @@ import React from 'react'
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import Navbar from './components/layout/Navbar'
 import Index from './components/layout/Index'
+import {Provider} from './context'
 
 const App = () => {
     return (
-        <Router>
-            <Navbar/>
-            <div className="container">
-                <Switch>
-                    <Route exact path="/" component={Index} />
-                </Switch>
-            </div>
-        </Router>
+        <Provider>
+            <Router>
+                <Navbar/>
+                <div className="container">
+                    <Switch>
+                        <Route exact path="/" component={Index} />
+                    </Switch>
+                </div>
+            </Router>
+        </Provider>
     )
 }
 
--- src/components/tracks/Tracks.js
+++ src/components/tracks/Tracks.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import {Consumer} from '../../context'
 
 class Tracks extends Component {
     render() {
         return (
-            <div>
-                tracks
-            </div>
+            <Consumer>
+                {value => {
+                    console.log(value)
+                }}
+            </Consumer>
         )
     }
 }
