6ceb1db797ae0c46960470dd914b39719bd9032f
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-05 18:32:01 +1000

    Api request get

--- src/context.js
+++ src/context.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 const Context = React.createContext()
 
 export class Provider extends Component {
     state = {
-        track_list: [
-            {track:{ track_name:'abc'}},
-            {track:{ track_name:'a2c'}},
-            {track:{ track_name:'ab3c'}},
-            {track:{ track_name:'a4bc'}},
-        ],
+        track_list: [],
         heading: 'Top 10 Tracks'
+    }
+
+    componentDidMount(){
+        axios.get(
+            `https://cors-anywhere.herokuapp.com/http://api.musixmatch.com/ws/1.1/chart.tracks.get?page=1&page_size=10&country=us&
+            f_has_lyrics=1&apikey=${
+                process.env.REACT_APP_MM
+            }`
+        )
+        .then(res => {
+            //console.log(res.data.message.body.track_list)
+            this.setState({tracklist:res.data.message.body.track_list})
+            }
+        )
+        .catch(err => console.log(err))
     }
 
     render() {
