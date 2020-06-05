03dba7ab688ef15fd90a29ca5518b4b7e5e4e1cf
kovacevicemir <emirkovacevic92@gmail.com>
2020-06-05 16:41:30 +1000

    initial, components created and react router

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-light bg-dark mb-5">
+            <span className="text-white brand mb-0 mx-auto h1">LyricFinder</span>
+        </nav>
+    )
+}
+
+export default Navbar
---
+++ src/components/tracks/Tracks.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+class Tracks extends Component {
+    render() {
+        return (
+            <div>
+                tracks
+            </div>
+        )
+    }
+}
+
+
+export default Tracks
